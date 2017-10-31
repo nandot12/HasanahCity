8a8d3f19d6a93d81f322802210f5806ce22eb24a
nandot12 <nandohusni93@gmail.com>
2017-10-31 23:31:00 +0700

    hehe
    hehe

--- src/main/java/com/krearive/hasanahnew/MapsActivity.java
+++ src/main/java/com/krearive/hasanahnew/MapsActivity.java
@@ -13,6 +13,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    //hello manda
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
